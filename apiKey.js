9a0b933e005ca4b6458023d3c492cef2da5038ac
Shahab Uddin Ghori <shahabnisar918@gmail.com>
2024-05-24 21:23:40 +0500

    footer added
    footer added with linktree

--- apiKey.js
+++ apiKey.js
@@ -1,60 +1,14 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const emoji = document.getElementById('emoji');
 
+  document.addEventListener('mousemove', (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
 
+      // Offset the emoji slightly so it doesn't sit directly under the cursor
+      const offsetX = 10;
+      const offsetY = 10;
 
-const form = document.querySelector("#weatherForm");
-const cityInput = document.querySelector("#inputField");
-// https://home.openweathermap.org/api_keys
-const API_KEY = "21b776b8f103d8742f43ac6f5928e618";
-const temp = document.querySelector("#temp");
-const humidity = document.querySelector("#humidity");
-const message = document.querySelector("#message");
-
-const formHandler = async (event) => {
-  try {
-    event.preventDefault(); // prevents page refresh
-
-    message.innerText = "";
-
-    const city = cityInput.value;
-
-    const response = await axios(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    );
-    const data = response.data; // await response.json();// converting data into JSON
-
-    temp.innerText = data.main.temp;
-    humidity.innerText = data.main.humidity;
-
-    console.log("🚀 ~ formHandler ~ response:", data);
-  } catch (err) {
-    console.log(err);
-    message.innerText = err?.response?.data?.message || "unknown error";
-  }
-};
-
-form.addEventListener("submit", formHandler);
-
-// error handling
-// try {
-//   try {
-//     dfdfdf;
-//   } catch (error) {
-//     karim;
-//   }
-// } catch (shehzad) {
-//   console.log("🚀 ~ shehzad:", shehzad);
-// }
-
-// console.log("code pura last tak chal raha hy");
-
-// try {
-//   const myApi = "https://api.github.com/users/shehdijdfijdza-d";
-
-//   const response = await axios(myApi);
-
-//   console.log("🚀 ~ response:", response.data);
-// } catch (error) {
-//   console.log(error.response.data.message);
-// }
-
-console.log("end");
+      emoji.style.transform = `translate(${mouseX + offsetX}px, ${mouseY + offsetY}px)`;
+  });
+});
